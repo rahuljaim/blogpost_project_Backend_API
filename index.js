e4a8bde06be8b20424c96cedd97159a20306c98c
Rahul Jain <rahuljait@gmail.com>
2022-02-11 13:59:07 +0530

    initial commit for api

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import BodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import postRoutes from "./routes/posts.js";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(BodyParser.json({ limit: "30mb", extended: true }));
+app.use(BodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/post", postRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`MongoDB Server is running on ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+// mongoose.set("useFindAndModify", false);
